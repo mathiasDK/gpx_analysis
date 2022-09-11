83371334b4995ad0c5576b59d9512e6ffb1c186f
Mathias Nørskov <bondnoerskov@hotmail.com>
2022-09-11 19:30:56 +0200

    load data

--- core/load_data.py
+++ core/load_data.py
@@ -21,8 +21,30 @@ def load_data(file_path: str) -> gpxpy.gpx.GPX:
 
 if __name__=='__main__':
     gpx = load_data('data/gpx_files/20220705_TMB1.gpx')
-    print(gpx.get_track_points_no())
-    print(gpx.get_elevation_extremes())
-    print(gpx.get_uphill_downhill())
+    # print(gpx.get_track_points_no())
+    # print(gpx.get_elevation_extremes())
+    # print(gpx.get_uphill_downhill())
 
-    print(type(gpx))
+    route_info = []
+    show_info = True
+
+    for track in gpx.tracks:
+        for segment in track.segments:
+            for point in segment.points:
+                if show_info:
+                    print(dir(point))
+                    show_info = False
+                
+                route_info.append({
+                    'latitude': point.latitude,
+                    'longitude': point.longitude,
+                    'elevation': point.elevation,
+                    'time': point.time
+                })
+
+    route_df = pd.DataFrame(route_info)
+    print(route_df.head())
+
+    
+
+    # print(type(gpx))
